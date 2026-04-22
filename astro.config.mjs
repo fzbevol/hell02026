d1dae418f190ce83f76e4179b6d2ce64df355f80
fzbevol <fzbevol@gmail.com>
2026-04-22 17:29:51 +0800

    FIX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hell02026.pages.dev',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [tailwind(), mdx(), sitemap()],
 });
